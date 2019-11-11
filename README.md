c5220624433172361051c533d8e5146882d44af4
Alfredo <alfredodomin91@gmail.com>
2019-11-10 19:17:50 -0600

    Hr update and Readme File

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Unit05_DailyPlanner
+
+
+# Daily Planner
+This is a simple calendar application, oriented to allow the user to save his events for each working hour in the day. It features dynamically updated HTML and CSS powered by jQuery
+
+[DailyPlanner](https://adoming8.github.io/Unit05_DailyPlanner/"DailyPlanner")
+
+## Getting Started
++ This Daily work hrs planner is created dynamically using jQuery and Bootstrap. At the topy of the Daily Planner the curernt date is displayed to the user. There is no pre-requisites or installation requiered to reun the application. You can simply clone the repository and have your file locally available.
+
+
+
--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 
 var listElement = document.getElementById("list");
 var sbtnElement = document.getElementById("savebtn");
-var saveButton = document.querySelector("#keeps");
+var saveButton = document.querySelector("#task");
 
-var hrs = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM","6PM","7PM","8PM","10PM","11PM","12AM","1PM","2PM","3PM","4PM","5PM","6PM","7PM","8PM"]
+var hrs = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM",]
 
 renderLastRegistered();
+
     hrs.forEach(function(elem,i){
      let htmlString =` 
      <div id="input-row" class="row"> 
         <div class="col-2 fecha id="datediv"><span> ${hrs[i]} </span></div>
         <div class="col-8" id="task-input">
-            <input type="text" id="task" placeholder="Daily Task" text=""> </div>
+            <input type="text" id="task" placeholder="Daily Task" text=" ">
+        </div>
         <div class="col-2" id="save_sec">
             <button type="button" class="btn btn-light id="keeps" >Save</button> </div>
      </div>
@@ -21,30 +23,29 @@ renderLastRegistered();
 
 // click event on save button - sending input to local memory based on "click"
 
+
 $(document).on("click", 'button', function(event) {
     event.preventDefault();
 
-    var toDo = $("#task").val();
+    toDo = $("#task").val();
 
+    localStorage.setItem("task", toDo); 
 
-        localStorage.setItem("task", toDo); // toDo type is a string
-        console.log(toDo);
+        console.log("this is todo var " + toDo);
     renderLastRegistered();
 });
 
 
 function renderLastRegistered() {
-   
-    // var Do = $("#task").val();
-   var Do = localStorage.getItem("task");
+    toDo = localStorage.getItem("task");
   
-    if (Do === null) {
-    // console.log("coming back 1", Do );
+    if (toDo === null) {
+    console.log("coming back 1 ", toDo );
         return;
     }
-    // console.log("coming back 2", Do );
+    console.log("coming back 2 ", toDo );
 
-   $('#task').html(`${Do}`);
+   $('#task').html("${toDo}");
   };
 
 
