ae472714f9f76c22679f4b6fb9770d426afb9b8a
Alfredo <alfredodomin91@gmail.com>
2019-11-13 23:09:55 -0600

    Hard Code HTMl & working local storage

--- script.js
+++ script.js
@@ -1,59 +1,71 @@
 
-var listElement = document.getElementById("list");
-var sbtnElement = document.getElementById("savebtn");
-var saveButton = document.querySelector("#task");
-
-var hrs = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM",]
-
-renderLastRegistered();
-
-    hrs.forEach(function(elem,i){
-     let htmlString =` 
-     <div id="input-row" class="row"> 
-        <div class="col-2 fecha id="datediv"><span> ${hrs[i]} </span></div>
-        <div class="col-8" id="task-input">
-            <input type="text" id="task" placeholder="Daily Task" text=" ">
-        </div>
-        <div class="col-2" id="save_sec">
-            <button type="button" class="btn btn-light id="keeps" >Save</button> </div>
-     </div>
-    `
- $("#list").append(htmlString);
-});
-
-// click event on save button - sending input to local memory based on "click"
-
-
-$(document).on("click", 'button', function(event) {
-    event.preventDefault();
-
-    toDo = $("#task").val();
-
-    localStorage.setItem("task", toDo); 
-
-        console.log("this is todo var " + toDo);
-    renderLastRegistered();
-});
-
-
-function renderLastRegistered() {
-    toDo = localStorage.getItem("task");
-  
-    if (toDo === null) {
-    console.log("coming back 1 ", toDo );
-        return;
-    }
-    console.log("coming back 2 ", toDo );
-
-   $('#task').html("${toDo}");
-  };
-
-
  // moment.js to set the current date
 
   let dateNo = moment();
 
-  let currentTime = dateNo.format("HH")
+  let currentTime = dateNo.format("Hr")
   currentTime = parseInt(currentTime)
 
   $("#currentDay").text(dateNo.format("dddd, MMMM DD"));
+
+
+
+// click event on save button - sending input to local memory based on "click"
+          // selecting hourly task inputs from HTML elements
+var firstuserinput_9AM = document.getElementById("first_toDo");
+var seconduserinput_10AM = document.getElementById("second_toDo");
+var thirduserinput_11AM = document.getElementById("third_toDo");
+var fourthuserinput_12PM = document.getElementById("fourth_toDo");
+var fifthuserinput_1PM = document.getElementById("fifth_toDo");
+var sixthuserinput_2PM = document.getElementById("sixth_toDo");
+var seventhuserinput_3PM = document.getElementById("seventh_toDo");
+var eighthuserinput_4PM = document.getElementById("eighth_toDo");
+var ninethuserinput_5PM = document.getElementById("nineth_toDo");
+
+
+$("button").on("click", function() {
+var toDo_obj = {                        // obj with user hourly input values
+     key_9AM: firstuserinput_9AM.value, 
+     key_10AM: seconduserinput_10AM.value,
+     key_11AM: thirduserinput_11AM.value,
+     key_12PM: fourthuserinput_12PM.value,
+     key_1PM: fifthuserinput_1PM.value,
+     key_2PM: sixthuserinput_2PM.value,
+     key_3PM: seventhuserinput_3PM.value,
+     key_4PM: eighthuserinput_4PM.value,
+     key_5PM: ninethuserinput_5PM.value,
+}
+//setting the input to a key value pair to save under local storage
+localStorage.setItem("toDo_9AM", toDo_obj.key_9AM);
+localStorage.setItem("toDo_10AM", toDo_obj.key_10AM); 
+localStorage.setItem("toDo_11AM", toDo_obj.key_11AM); 
+localStorage.setItem("toDo_12PM", toDo_obj.key_12PM);
+localStorage.setItem("toDo_1PM", toDo_obj.key_1PM); 
+localStorage.setItem("toDo_2PM", toDo_obj.key_2PM); 
+localStorage.setItem("toDo_3PM", toDo_obj.key_3PM); 
+localStorage.setItem("toDo_4PM", toDo_obj.key_4PM); 
+localStorage.setItem("toDo_5PM", toDo_obj.key_5PM); 
+})
+
+ var toDo_9amlocalstorage = localStorage.getItem("toDo_9AM");
+ var toDo_10amlocalstorage = localStorage.getItem("toDo_10AM");
+ var toDo_11amlocalstorage = localStorage.getItem("toDo_11AM");
+ var toDo_12pmlocalstorage = localStorage.getItem("toDo_12PM");
+ var toDo_1pmlocalstorage = localStorage.getItem("toDo_1PM");
+ var toDo_2pmlocalstorage = localStorage.getItem("toDo_2PM");
+ var toDo_3pmlocalstorage = localStorage.getItem("toDo_3PM");
+ var toDo_4pmlocalstorage = localStorage.getItem("toDo_4PM");
+ var toDo_5pmlocalstorage = localStorage.getItem("toDo_5PM");
+
+
+firstuserinput_9AM.value = toDo_9amlocalstorage; // show store data on page
+console.log(toDo_9amlocalstorage)
+seconduserinput_10AM.value = toDo_10amlocalstorage; 
+thirduserinput_11AM.value = toDo_11amlocalstorage; 
+fourthuserinput_12PM.value = toDo_12pmlocalstorage; 
+fifthuserinput_1PM.value = toDo_1pmlocalstorage; 
+sixthuserinput_2PM.value = toDo_2pmlocalstorage; 
+seventhuserinput_3PM.value = toDo_3pmlocalstorage; 
+eighthuserinput_4PM.value = toDo_4pmlocalstorage; 
+ninethuserinput_5PM.value = toDo_5pmlocalstorage; 
+
